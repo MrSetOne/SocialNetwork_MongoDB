3797501407247c1122dcb85c4ebef90537ef8a95
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-31 21:03:20 +0200

    middelware works

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@ const user = require('../models/User');
 const bcrypt = require('bcryptjs')
 
 const userController = {
-    async create(req, res) {
+    async create(req, res, next) {
         try {
             if (req.body.password) {
                 req.body.role = "user";
@@ -14,30 +14,8 @@ const userController = {
                 res.status(400).send('Es necesario introducir una contraseña')
             }
         } catch (error) {
-            error.origin = "user"
-            if (error.errors) {
-                error.feedback = []
-                for (const fail in error.errors) {
-                    console.log(error.errors[fail].path);
-                    error.feedback.push(error.errors[fail].path)
-                };
-                res.status(400).send({
-                    message: 'Faltan campos por rellenar',
-                    feedback: error.feedback
-                })
-            } else if (error.keyPattern) {
-                const failed = Object.keys(error.keyPattern)
-                const failedValue = error.keyValue[failed[0]]
-                res.status(400).send({
-                    message: `El ${failed[0]} ${failedValue} ya está en uso.`,
-                    failedField: failed[0],
-                    failedValue
-                });
-
-            }
-            // console.log(Object.keys(error.errors).length)
-
-
+            error.origin = "user";
+            next(error);
         }
     }
 }
--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const { dbConnection } = require("./config/config")
+const { dbConnection } = require("./config/config");
+const { TypeError } = require("./middelwares/errors");
 
 app.use(express.json())
 dbConnection()
 
 app.use('/users', require('./routes/users'));
 
+
+app.use(TypeError)
 
 app.listen(port, console.log(`Server started on port ${port}`));
