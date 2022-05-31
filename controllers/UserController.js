34ade261af6550615b7ba3677a196de23e25393c
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-31 17:52:06 +0200

    middelware mid

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,43 @@
 const user = require('../models/User');
+const bcrypt = require('bcryptjs')
 
 const userController = {
     async create(req, res) {
         try {
-            const newUser = await user.create(req.body)
-            res.status(201).send({ message: `Se ha creado el usuario ${req.body.username}`, newUser });
+            if (req.body.password) {
+                req.body.role = "user";
+                req.body.confirmed = false;
+                req.body.password = await bcrypt.hash(req.body.password, 10)
+                const newUser = await user.create(req.body)
+                res.status(201).send({ message: `Se ha creado el usuario ${req.body.username}`, newUser });
+            } else {
+                res.status(400).send('Es necesario introducir una contraseña')
+            }
         } catch (error) {
-            console.error(error);
-            res.status(500).send(`No se ha podido crear el usuario D:`)
+            error.origin = "user"
+            if (error.errors) {
+                error.feedback = []
+                for (const fail in error.errors) {
+                    console.log(error.errors[fail].path);
+                    error.feedback.push(error.errors[fail].path)
+                };
+                res.status(400).send({
+                    message: 'Faltan campos por rellenar',
+                    feedback: error.feedback
+                })
+            } else if (error.keyPattern) {
+                const failed = Object.keys(error.keyPattern)
+                const failedValue = error.keyValue[failed[0]]
+                res.status(400).send({
+                    message: `El ${failed[0]} ${failedValue} ya está en uso.`,
+                    failedField: failed[0],
+                    failedValue
+                });
+
+            }
+            // console.log(Object.keys(error.errors).length)
+
+
         }
     }
 }
--- models/User.js
+++ models/User.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose') //Nos traemos los metodos de mongoose
 
 const userSchema = new mongoose.Schema({ //Definimos el tipo de dato que va a contener
-    username: String,
-    email: String,
+    username: {
+        type: String,
+        required: [true, 'es necesario introducir un Username'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'es necesario introducir un email'],
+        unique: true
+    },
     password: String,
     img: String,
-    confirmed: Boolean
+    confirmed: Boolean,
+    role: String
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema); //Generamos el modelo como tal
