d54ce180baaac892e32d9794e23a4e22ebd36291
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-01 23:39:51 +0200

    authentication middelware added

--- middelwares/authentication.js
+++ middelwares/authentication.js
@@ -1,11 +1,27 @@
-// const { jwt_secret } = require("../config/keys")
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { jwt_secret } = require('../config/keys');
+const User = require('../models/User');
 
+const authentication = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, jwt_secret);
+        const user = User.findOne({
+            _id: payload.id,
+            token
+        });
+        if (!user) {
+            return res.status(400).send('No estas autorizado')
+        };
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send({
+            message: 'Al parecer ha habido un error con el token',
+            error
+        })
+    }
+};
 
-// const authentication = async(req, res, next) => {
-//     try {
-//         const payload = jwt.verify(req.header.authorization, jwt_secret)
-//     } catch (error) {
-
-//     }
-// }
+module.exports = { authentication }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/UserController');
+const { authentication } = require('../middelwares/authentication');
 const router = express.Router();
 
 router.post('/', userController.create);
