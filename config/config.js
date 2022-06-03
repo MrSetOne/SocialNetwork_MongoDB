b01ba3a9a3e960f9f2c1614abd08806cdf0f5d60
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-03 14:29:01 +0200

    config update

--- config/config.js
+++ config/config.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose"); //Nos importamos mongoose para usarlo en la funcion dbConnection
 
 require("dotenv").config();
-const { MONGO_URI } = process.env
+const { MONGO_URI_TEST, MONGO_URI, DB_TEST } = process.env
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(MONGO_URI); //Metodo que se intenta conectar a la BBDD
+        if (DB_TEST == 'true') {
+            await mongoose.connect(MONGO_URI_TEST); //Metodo que se intenta conectar a la BBDD
+            console.log('Te has conectado al test');
+        } else {
+            await mongoose.connect(MONGO_URI);
+            console.log('te as conectado al deploy');
+        }
         console.log("BBDD conectada con éxito");
     } catch (error) {
         console.error(error);
