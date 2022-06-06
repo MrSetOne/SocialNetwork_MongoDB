04ca728c5626fe26dd51fcbac3b1f73766a04f40
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-06 14:05:14 +0200

    pending test

--- middelwares/deleters.js
+++ middelwares/deleters.js
@@ -6,8 +6,8 @@ const deleterPost = async(req, res, next) => {
     try {
         const target = await Post.findById(req.params._id);
         if (target.likes) {
-            target.likes.forEach(async(item) => {
-                await User.findByIdAndUpdate(item, { $pull: { likedPosts: req.params._id } });
+            target.likes.forEach(async(like) => {
+                await User.findByIdAndUpdate(like, { $pull: { likedPosts: req.params._id } });
             });
         };
         if (target.comments) {
@@ -21,6 +21,57 @@ const deleterPost = async(req, res, next) => {
         next();
     } catch (error) {
         return res.send({ message: 'Algo ha salido mal en el middelware', error })
+    }
+}
+
+const deleterUser = async(req, res, next) => {
+    try {
+        const target = await User.findById(req.params._id);
+        if (target.postIds) {
+            target.postIds.forEach(async(post) => {
+                const targetPost = await Post.findById(post);
+                if (targetPost.likes) {
+                    targetPost.likes.forEach(async(like) => {
+                        await User.findByIdAndUpdate(like, { $pull: { likedPosts: targetPost._id } });
+                    })
+                }
+                if (targetPost.comments) {
+                    targetPost.comments.forEach(async(comment) => {
+                        const toDelete = await Comment.findById(comment);
+                        await User.findByIdAndUpdate(toDelete.author, { $pull: { comments: comment } });
+                        await Comment.findByIdAndDelete(comment);
+                    })
+                }
+            })
+        };
+        target = await User.findById(req.params._id);
+        if (target.comments) {
+            target.comments.forEach(async(comment) => {
+                const toDelete = await Comment.findById(comment);
+                await Post.findByIdAndUpdate(toDelete.postId, { $pull: { comments: comment } });
+                await Comment.findByIdAndDelete(comment);
+            })
+        };
+        target = await User.findById(req.params._id);
+        if (target.followers) {
+            target.followers.forEach(async(follower) => {
+                await User.findByIdAndUpdate(follower, { $pull: { following: target._id } })
+            })
+        };
+        target = await User.findById(req.params._id);
+        if (target.following) {
+            target.following.forEach(async(follow) => {
+                await User.findByIdAndUpdate(follow, { $pull: { followers: target._id } })
+            })
+        };
+        target = await User.findById(req.params._id);
+        if (target.likedPosts) {
+            target.likedPosts.forEach(async(like) => {
+                await Post.findByIdAndUpdate(like, { $pull: { likes: target._id } })
+            })
+        }
+    } catch (error) {
+        res.send(error)
     }
 }
 
