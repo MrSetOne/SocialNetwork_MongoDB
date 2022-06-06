e3eacb8f2d57397c98439a79c131f650be2289c4
MrSetOne <lara.sanchez.michael@gmail.com>
2022-06-06 12:05:52 +0200

    cascada de comentarios

--- middelwares/deleters.js
+++ middelwares/deleters.js
@@ -3,27 +3,18 @@ const User = require("../models/User")
 const Comment = require("../models/Comment")
 
 const deleterPost = async(req, res, next) => {
-
     try {
-        console.log("entramos en middelware");
         const target = await Post.findById(req.params._id);
-        console.log('el target es:');
-        console.log(target);
-        console.log(target.likes[0]);
-        console.log('En teoria se ha borrado del perfil del autor');
         if (target.likes) {
-            console.log('Ha entrado a borrar los likes');
             await target.likes.forEach(item => {
-                console.log("estas dentro del bucle");
                 User.findByIdAndUpdate(item, { $pull: { likedPosts: req.params._id } }, { new: true });
             });
         };
         if (target.comments) {
-            console.log('Entras a borrar los comentarios');
-            await target.comments.forEach((comment, i) => {
-                const toDelete = Comment.findById(comment);
-                User.findByIdAndUpdate(toDelete.author, { $pull: { comments: comment } })
-                Comment.findByIdAndDelete(comment);
+            target.comments.forEach(async(comment) => {
+                const toDelete = await Comment.findById(comment.toString());
+                await User.findByIdAndUpdate(toDelete.author, { $pull: { comments: comment } })
+                await Comment.findByIdAndDelete(comment);
             })
         }
         await User.findByIdAndUpdate(target.userId, { $pull: { postIds: req.params._id } });
